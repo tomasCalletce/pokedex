4e8e44f01590e8b0f31c9aff76a8269b1eec1e9b
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-11-07 11:10:45 -0500

    Port update

--- src/main.js
+++ src/main.js
@@ -14,6 +14,6 @@ app.get('/', homeController.index);
 
 app.get('/all',homeController.all);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
